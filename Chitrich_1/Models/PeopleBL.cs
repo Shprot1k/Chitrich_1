f6dc78ee878e18a4da1d9be014181f8fa17dfc97
tarasshpota@gmail.com <tarasshpota@gmail.com>
2024-07-03 20:42:03 +0300

    Everything is rewritten

--- Chitrich_1/Models/PeopleBL.cs
+++ Chitrich_1/Models/PeopleBL.cs
@@ -1,201 +1,319 @@
-﻿using DocumentFormat.OpenXml.Packaging;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Text;
 
 namespace Chitrich_1.Models
 {
     internal class PeopleBL
     {
-        public static List<People> Read(string filePath, bool firstRowIsHeader = true)
+        public static List<People> Read(string fileName)
         {
-            List<People> peoples = new List<People>();
-            //List<string> Headers = new List<string>();
-            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, false))
+            List < People > peoples = new List < People >();
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
             {
-                //Read the first Sheets 
-                Sheet sheet = doc.WorkbookPart.Workbook.Sheets.GetFirstChild<Sheet>();
-                Worksheet worksheet = (doc.WorkbookPart.GetPartById(sheet.Id.Value) as WorksheetPart).Worksheet;
-                IEnumerable<Row> rows = worksheet.GetFirstChild<SheetData>().Descendants<Row>();
-                
-                
-                int counter = 0;
-                foreach (Row row in rows)
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                Sheet sheet = workbookPart.Workbook.Descendants<Sheet>().FirstOrDefault();
+                if (sheet == null)
                 {
-                    //Read the first row as header
-                    if (counter == 0 && firstRowIsHeader ==true)
+                    throw new Exception("Sheet not found");
+                }
+                WorksheetPart worksheetPart = (WorksheetPart)workbookPart.GetPartById(sheet.Id);
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+                int rowNum = 0;
+                foreach (Row row in sheetData.Elements<Row>())
+                {
+                    People p = new People();
+                    int cellNum = 0;
+                    if (rowNum > 0)
                     {
-                        /* поки непотрібно
-                        var j = 1;
-                        foreach (Cell cell in row.Descendants<Cell>())
+                        foreach (Cell cell in row.Elements<Cell>())
                         {
-                            var colunmName = firstRowIsHeader ? GetCellValue(doc, cell) : "Field" + j++;
-                            Console.WriteLine(colunmName);
-                            Headers.Add(colunmName);
-                            dt.Columns.Add(colunmName);                            
-                        }
-                        */
-                    }
-                    else
-                    {
-                        People people = new People();
-                        int i = 0;
-                        foreach (Cell cell in row.Descendants<Cell>())
-                        {
-                            switch (i)
+                            string cellValue = GetCellValue(spreadsheetDocument, cell);
+                            switch (cellNum)
                             {
                                 case 0:
-                                    //people.Id = int.Parse(GetCellValue(doc, cell));
-                                    people.Id = int.Parse(cell?.CellValue?.Text);
-                                    i++;
+                                    p.Id = int.Parse(cellValue);
+                                    cellNum++;
                                     break;
                                 case 1:
-                                    people.Name = GetCellValue(doc, cell);
-                                    i++;
+                                    p.Name = cellValue;
+                                    cellNum++;
                                     break;
                                 case 2:
-                                    people.Age = int.Parse(GetCellValue(doc, cell));
-                                    i++;
+                                    p.Age = int.Parse(cellValue);
+                                    cellNum++;
                                     break;
                                 case 3:
-                                    people.Salary = int.Parse(GetCellValue(doc, cell));
-                                    i++;
+                                    p.Salary = int.Parse(cellValue);
+                                    cellNum++;
                                     break;
                                 case 4:
-                                    people.Department = GetCellValue(doc, cell);
-                                    i++;
+                                    p.Department = cellValue;
+                                    cellNum++;
                                     break;
-
+                                default:
+                                    cellNum++; break;
                             }
-
                         }
-                        peoples.Add(people);
-                        /* ісходнік, якщо є в коміті то забув видалити
-                        dt.Rows.Add();
-                        int i = 0;
-                        foreach (Cell cell in row.Descendants<Cell>())
-                        {
-                            dt.Rows[dt.Rows.Count - 1][i] = GetCellValue(doc, cell);
-                            i++;
-                        }
-                        */
+                        peoples.Add(p);
                     }
-                    counter++;
+                    rowNum++;
                 }
-
             }
             return peoples;
         }
 
         public static void Print(List<People> peoples)
         {
+            Console.WriteLine();
             foreach (People people in peoples)
             {
-                Console.WriteLine(people.Id + "\t"
-                    + people.Name + "\t"
-                    + people.Age + "\t"
-                    + people.Salary + "\t"
-                    + people.Department);
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", people.Id, people.Name, people.Age, people.Salary, people.Department);
             }
         }
 
-        private static string GetCellValue(SpreadsheetDocument doc, Cell cell)
+        public static List<People> Sort(List<People> peoples)
         {
-            string value = cell.CellValue.InnerText;
-            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            Console.Write("\nSelect a sorting method:\n"
+                + "1 - By Id\n"
+                + "2 - By Name (dont work)\n"
+                + "3 - By Age\n"
+                + "4- By Salare\n"
+                + "5 - By Department (dont work)\n"
+                + "0 - Don't sort\n"
+                + "Your choice: ");
+            int sortOption = int.Parse(Console.ReadLine());
+            switch (sortOption)
             {
-                //return doc.WorkbookPart.SharedStringTablePart.SharedStringTable.ChildElements.GetItem(int.Parse(value)).InnerText;
-                return doc.WorkbookPart.SharedStringTablePart.SharedStringTable.ChildElements.ToString();
-            }
-            return value;
-        }
+                case 0: // Don't sort
+                    return peoples;
 
-        /*
-        Workbook wb = new Workbook(filePath);
-        WorksheetCollection collection = wb.Worksheets;
-        List<People> peoples = new List<People>();
-        for (int worksheetIndex = 0; worksheetIndex < collection.Count; worksheetIndex++)
-        {
-            Worksheet worksheet = collection[worksheetIndex];
-            int rows = worksheet.Cells.MaxDataRow;
-            int cols = worksheet.Cells.MaxDataColumn;
-            for (int row = 1; row < rows; row++)
-            {
-                People people = new People();
-                for (int column = 0; column < cols; column++)
-                {
-                    if (column == 0)
+                case 1: // Id
+                    for (int i = 0; i < peoples.Count; i++)
                     {
-                        people.Id = int.Parse(worksheet.Cells[row, column].Value.ToString());
+                        for (int j = 0; j < peoples.Count; j++)
+                        {
+                            if (peoples[i].Id < peoples[j].Id)
+                            {
+                                var temp = peoples[i];
+                                peoples[i] = peoples[j];
+                                peoples[j] = temp;
+                            }
+                        }
+                    }
+                    return peoples;
 
-                    }else if (column == 1)
-                    {
-                        people.Name = worksheet.Cells[row, column].Value.ToString();
-                    }else if (column == 2)
-                    {
-                        people.Age = int.Parse(worksheet.Cells[row, column].Value.ToString());
-                    }
-                    else if (column == 3)
-                    {
-                        people.Salary = int.Parse(worksheet.Cells[row, column].Value.ToString());
-                    }else if (row == 4)
-                    {
-                        people.Department = worksheet.Cells[row, column].Value.ToString();
-                    }
-                }
-                peoples.Add(people);
-            }
-        }
-        return peoples;  
-        */
-    }
-        /*
+                case 2: // By Name (dont work)
+                    return peoples;
 
-        
-        
-        public static List<People> SortByAge(List<People> peoples)
-        {
-            for (int i = 0; i < peoples.Count; i++)
-            {
-                for (int j = 0; j < peoples.Count; j++)
-                {
-                    if (peoples[i].Age < peoples[j].Age)
+                case 3: // By Age
+                    for (int i = 0; i < peoples.Count; i++)
                     {
-                        var temp = peoples[i];
-                        peoples[i] = peoples[j];
-                        peoples[j] = temp;
+                        for (int j = 0; j < peoples.Count; j++)
+                        {
+                            if (peoples[i].Age < peoples[j].Age)
+                            {
+                                var temp = peoples[i];
+                                peoples[i] = peoples[j];
+                                peoples[j] = temp;
+                            }
+                        }
                     }
-                }
+                    return peoples;
+
+                case 4: // By Salare
+                    for (int i = 0; i < peoples.Count; i++)
+                    {
+                        for (int j = 0; j < peoples.Count; j++)
+                        {
+                            if (peoples[i].Salary < peoples[j].Salary)
+                            {
+                                var temp = peoples[i];
+                                peoples[i] = peoples[j];
+                                peoples[j] = temp;
+                            }
+                        }
+                    }
+                    return peoples;
+
+                case 5: //By Department (dont work)
+                    return peoples;
+                default: 
+                    return peoples;
+
+
             }
-            return peoples;
         }
 
         public static void Save(List<People> peoples)
         {
-            
-            Workbook workbook = new Workbook();
-            Worksheet worksheet = workbook.Worksheets[0];
-
-            worksheet.Cells[0, 0].PutValue("ID");
-            worksheet.Cells[0, 1].PutValue("Name");
-            worksheet.Cells[0, 2].PutValue("Age");
-            worksheet.Cells[0, 3].PutValue("Salary");
-            worksheet.Cells[0, 4].PutValue("Department");
-
-            for (int i = 0; i < peoples.Count; i++)
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Create("output.xlsx", SpreadsheetDocumentType.Workbook))
             {
-                worksheet.Cells[i + 1, 0].PutValue(peoples[i].Id);
-                worksheet.Cells[i + 1, 1].PutValue(peoples[i].Name);
-                worksheet.Cells[i + 1, 2].PutValue(peoples[i].Age);
-                worksheet.Cells[i + 1, 3].PutValue(peoples[i].Salary);
-                worksheet.Cells[i + 1, 4].PutValue(peoples[i].Department);
+                WorkbookPart workbookPart = spreadsheetDocument.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new Worksheet(new SheetData());
+                Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
+                Sheet sheet = new Sheet()
+                {
+                    Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                    SheetId = 1,
+                    Name = "Sheet1"
+                };
+                sheets.Append(sheet);
+                SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+                
+                int choise = 1;
+                List<int> choises = new List<int>();
+                
+                while (choise != 0)
+                {
+                    Console.Write("Select the columns to save\n"
+                        + "1 - Id\n"
+                        + "2 - Name\n"
+                        + "3 - Age\n"
+                        + "4 - Salary\n"
+                        + "5 - Department\n"
+                        + "Else - exit\n"
+                        + "Your choise: ");
+                    choise = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    if (choise >= 1 && choise <= 5)
+                    {
+                        choises.Add(choise);
+                    }
+                    else
+                    {
+                        choise = 0;
+                    }
+                }
+                Row headerRow = new Row();
+                int i = 1;
+                foreach (int chois in choises)
+                {
+                    switch (chois)
+                    {
+                        case 1:
+                            headerRow.Append(CreateTextCell(CellNum(i), "Id"));
+                            break;
+                        case 2:
+                            headerRow.Append(CreateTextCell(CellNum(i), "Name"));
+                            break;
+                        case 3:
+                            headerRow.Append(CreateTextCell(CellNum(i), "Age"));
+                            break;
+                        case 4:
+                            headerRow.Append(CreateTextCell(CellNum(i), "Salary"));
+                            break;
+                        case 5:
+                            headerRow.Append(CreateTextCell(CellNum(i), "Department"));
+                            break;
+                    }
+                    i++;
+                }
+                
+                sheetData.AppendChild(headerRow);
+                foreach (People people in peoples)
+                {
+                    Row dataRow = new Row();
+                    foreach (int chois in choises)
+                    {
+                        switch (chois)
+                        {
+                            case 1:
+                                dataRow.Append(CreateIntCell(null, people.Id));
+                                break;
+                            case 2:
+                                dataRow.Append(CreateTextCell(null, people.Name));
+                                break;
+                            case 3:
+                                dataRow.Append(CreateIntCell(null, people.Age));
+                                break;
+                            case 4:
+                                dataRow.Append(CreateIntCell(null, people.Salary));
+                                break;
+                            case 5:
+                                dataRow.Append(CreateTextCell(null, people.Department));
+                                break;
+                        }
+                    }
+                    sheetData.AppendChild(dataRow);
+                }
+
+                workbookPart.Workbook.Save();
             }
-            workbook.Save("output.xlsx", SaveFormat.Xlsx);
-        }    
+        }
+
+        private static string GetCellValue(SpreadsheetDocument document, Cell cell)
+        {
+            string value = cell.CellValue.InnerText;
+            
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                SharedStringTablePart stringTable = document.WorkbookPart.SharedStringTablePart;
+                return stringTable.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
+            }
+            else if (cell.DataType != null && cell.DataType.Value == CellValues.Boolean)
+            {
+                return value == "0" ? "FALSE" : "TRUE";
+            }
+            else
+            {
+                return value;
+            }
+        }
+        private static Cell CreateTextCell(string cellReference, string cellValue)
+        {
+            Cell cell = new Cell()
+            {
+                DataType = CellValues.String,
+                CellValue = new CellValue(cellValue)
+            };
+
+            if (!string.IsNullOrEmpty(cellReference))
+            {
+                cell.CellReference = cellReference;
+            }
+
+            return cell;
+        }
+        private static Cell CreateIntCell(string cellReference, int cellValue)
+        {
+            Cell cell = new Cell()
+            {
+                DataType = CellValues.Number,
+                CellValue = new CellValue(cellValue)
+            };
+
+            if (!string.IsNullOrEmpty(cellReference))
+            {
+                cell.CellReference = cellReference;
+            }
+
+            return cell;
+        }
+
+        private static string CellNum(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    return "A1";
+                case 2:
+                    return "B1";
+                case 3:
+                    return "C1";
+                case 4:
+                    return "D1";
+                case 5:
+                    return "E1";
+                default:
+                    return "Eror";
+            }
+        }
     }
-    */
-    
 }
--- Chitrich_1/Program.cs
+++ Chitrich_1/Program.cs
@@ -6,18 +6,48 @@ namespace Chitrich_1
     {
         static void Main(string[] args)
         {
-            /*
-            Console.Write("Name of the file to be read: ");
-            //var peoples_0 = PeopleBL.Read(Console.ReadLine());
-            var peoples_0 = PeopleBL.Read("generated_excel_data.xlsx");
-            PeopleBL.Print(peoples_0);
-            Console.WriteLine("===========================================================");
-            var peoples_1 = PeopleBL.SortByAge(peoples_0);
-            PeopleBL.Print(peoples_1);
-            PeopleBL.Save(peoples_1);
-            */
-            var peoples_0 = PeopleBL.Read("generated_excel_data.xlsx");
-            PeopleBL.Print(peoples_0);
+            List<People> p = new List<People>();
+            int choise = 1;
+            while (choise != 0)
+            {
+                Console.Write("Select an action\n"
+                    + "1 - Read file   generated_excel_data.xlsx\n"
+                    + "2 - Sort\n"
+                    + "3 - Print\n"
+                    + "4 - Save\n"
+                    + "5 - Clear console\n"
+                    + "0 - Exit\n"
+                    + "Your choise: ");
+                choise = int.Parse(Console.ReadLine());
+
+                switch (choise)
+                {
+                    case 1: // Read file
+                        Console.Write("Name of the file to be read: ");
+                        p = PeopleBL.Read(Console.ReadLine());
+                        Console.WriteLine("File read\n");
+                        break;
+                    case 2: // Sort
+                        p = PeopleBL.Sort(p);
+                        Console.WriteLine("List sorted\n");
+                        break;
+                    case 3: // Print
+                        PeopleBL.Print(p);
+                        Console.WriteLine();
+                        break;
+                    case 4: // Save
+                        PeopleBL.Save(p);
+                        Console.WriteLine("File saved\n");
+                        break;
+                    case 5: // Clear console
+                        Console.Clear();
+                        break;
+                    case 0: // Exit
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
